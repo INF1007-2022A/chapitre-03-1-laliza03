bf4ed49383ffbba933edc6853e680bf9e49dbf73
laliza03 <112522886+laliza03@users.noreply.github.com>
2022-10-18 15:46:24 -0400

    FINI!

--- exercice.py
+++ exercice.py
@@ -1,35 +1,35 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 import math
 
+
+
 def square_root(a: float) -> float:
-    return 0.0
+    return a**0.5
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a**2
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a+b+c)/3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return math.radians(angle_degs + (angle_mins + (angle_secs/60))/ 60)
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
-
+    degrees = math.degrees(angle_rads)
+    minutes = (degrees - math.floor(degrees))*60
+    secondes = (minutes - math.floor(minutes))*60
+    return math.floor(degrees), math.floor(minutes), math.floor(secondes)
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32)/ 1.8
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return (temperature *1.8)+ 32
 
 
 def main() -> None:
@@ -60,68 +60,4 @@ if __name__ == '__main__':
 
 
 
-#Ma version de l'exercice chapitre #3.1
-    
-    
-    
-    #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
-import math
-pi=math.pi
-
-def square_root(a: float) -> float:
-    return a**0.5
-
-
-def square(a: float) -> float:
-    return a**2
-
-
-def average(a: float, b: float, c: float) -> float:
-    return (sum((a,b,c)))/3
-
-
-
-def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-  import math
-  def radian_converter(angle_degs):
-      pi_value=math.pi
-      radian=(angle_degs*pi_value)/180
-      return radian_converter
-
-
-def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
-
-
-def to_celsius(temperature: float) -> float:
-    return 0.0
-
-
-def to_farenheit(temperature: float) -> float:
-    return 0.0
-
-
-def main() -> None:
-    print(f"La racine carré de 144 est : {square_root(144)}")
-
-    print(f"Le carré de 12 est : {square(12)}")
-
-    print(f"Moyenne des nombres 2, 4, 6: {average(2, 4, 6)}")
-
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
-
-    degrees, minutes, seconds = to_degrees(1.0)
-    print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
-
-    print(f"Conversion de 100 Celsius en Farenheit: {to_farenheit(100.0)}")
-    print(f"Conversion de 451 Farenheit en Celsius: {to_celsius(451.0)}")
-
-
-if __name__ == '__main__':
-    main()
-
-
-    
